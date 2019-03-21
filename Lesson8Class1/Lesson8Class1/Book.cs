56d6db887166ec9a01e6f10682ac77fdf81727a6
FiLoffs <filod72@gmail.com>
2019-03-21 07:20:47 +0200

    Task start

---
+++ Lesson8Class1/Lesson8Class1/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8Class1
+{
+    class Book
+    {
+        string title;
+        string []authors;
+        string puplisher;
+        ushort pages;
+        ushort year;
+
+        public string Title { get => title; set => title = value; }
+        public string Authors { get => authors; set => authors = value; }
+        public string Puplisher { get => puplisher; set => puplisher = value; }
+        public ushort Pages { get => pages; set => pages = value; }
+        public ushort Year { get => year; set => year = value; }
+
+        public Book(string title, string authors, string puplisher, ushort pages, ushort year)
+        {
+            this.title = title;
+            this.authors = authors;
+            this.puplisher = puplisher;
+            this.pages = pages;
+            this.year = year;
+        }
+    }
+    
+
+
+
+
+}
